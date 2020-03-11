b5b0e0509cba766b20d0fa3c4a723df2cb09d252
bnalive <p.bnalive@gmail.com>
2020-03-11 12:58:21 +0700

    Update bot.php
    change token

--- bot.php
+++ bot.php
@@ -2,8 +2,8 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'ouhqskdRP/sUP8uwpjAadPDJz6rj1Y3IR0/ZznmHBgsPmYq6Q+hzdEJ4OXgyw/8NaLy6GLAZYYbLhF/7S6i8K07k3yxT0sWcMEa6ixgJ2c0XIOEKRfUEQAsHVi4PbQU4HEk9GOq/cmdR3iRkQE9e5gdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = 'ba6e01c3eb0671a32e7d9fb3dbabd67d';
+$ACCESS_TOKEN = 'jkT73l6R8lr/SXW+XgfZMM1sYsrCDM2f83eTFfp2pT+ze9jyi5H+LtJeFXN1F1dRv5cg9g6ur5AvV+6cdEkuOyG/+yzBbisfsxnHXiCxJkTe0/qCEU/LiQO9VqdwFxVtBJow4HKONCimHSGyuc2LmQdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = '033772c31790f5a6587121b093ce751c';
 
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
